18946a39a1cb9164abe44b844ed372f4cd4be89b
Bernhard Posselt <dev@bernhard-posselt.com>
2014-04-13 01:41:50 +0200

    kasimir nav

--- kasimir/urls.py
+++ kasimir/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name="categories.html")),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^categories$', TemplateView.as_view(template_name="categories.html")),
 )
